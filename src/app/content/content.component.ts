b5e97d5c9516be12fa8bd85bb854d43b0abbf3ea
RoloufGoudebin <goudeloufrobin@gmail.com>
2020-09-02 23:44:03 +0200

    add src

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.loadScript('../assets/js/main.js');
+    this.loadScript('../assets/js/jquery.min.js');
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement>document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
+
+
+}
