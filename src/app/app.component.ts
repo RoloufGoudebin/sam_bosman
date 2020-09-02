b78b4863cc220cec96254153fe6ec6bff82b5585
RoloufGoudebin <goudeloufrobin@gmail.com>
2020-09-02 23:43:41 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bosman';
+
+
+  ngOnInit() {
+    this.loadScript('../assets/js/jquery.min.js');
+  	this.loadScript('../assets/js/aos.js');
+    this.loadScript('../assets/js/main.js')
+    
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement>document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
+
 }
