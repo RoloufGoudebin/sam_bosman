c35a211f74c5e5227c5e9510a08ffce2074de725
Robin Goudelouf <goudeloufrobin@gmail.com>
2021-08-21 13:24:30 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BioComponent } from './bio/bio.component';
+import { VideoComponent } from './video/video.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProComponent } from './pro/pro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'bio', component: BioComponent},
+  { path: 'video', component: VideoComponent},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'agenda', component: AgendaComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'pro', component: ProComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BioComponent } from './bio/bio.component';
+import { VideoComponent } from './video/video.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProComponent } from './pro/pro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BioComponent,
+    VideoComponent,
+    GalleryComponent,
+    AgendaComponent,
+    ContactComponent,
+    ProComponent
   ],
   imports: [
     BrowserModule,
